e2abc39d5c738dcaf5bc773b3a697f68f147458b
Randy Burnet <rrw.burnet@gmail.com>
2020-01-06 00:06:18 +0100

    test dynamic values inside template

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-
+const Tournament = require('../models/Tournament');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('layouts/main',{ title: 'Express', someVariable: 'example' });
+router.get('/', async (req, res, next) => {
+  const tournament = await Tournament.find();
+
+  res.render('layouts/main',{ title: 'Express', someVariable: 'example', count: tournament.length });
 });
 
 module.exports = router;
