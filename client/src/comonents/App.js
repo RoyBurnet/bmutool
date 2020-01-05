03562e422828bad84827c6a77c9cc4addca50f26
Randy Burnet <rrw.burnet@gmail.com>
2020-01-05 20:40:06 +0100

    draft

--- client/src/comonents/App.js
+++ client/src/comonents/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Router, Route, Switch} from 'react-router-dom';
+import history from '../history'
 
 function App() {
   return (
-    <div>
-      hello world
+    <div className="ui container">
+        <h1>App</h1>
+        <p>hello world</p>
     </div>
   )
 }
--- controllers/views.js
+++ controllers/views.js
@@ -1,10 +1,13 @@
 const asyncHandler = require('../middleware/async');
 const Tournament = require('../models/Tournament');
 
-exports.home = asyncHandler(async(req, res) => {
+exports.home = asyncHandler(async (req, res) => {
   const tournament = await Tournament.find();
-  
-  res.render('home', { title: 'Hey', message: 'Hello there!', count: tournament.length, data: tournament})
-})
 
-
+  res.render('home', {
+    title: 'Hey',
+    message: 'Hello there!',
+    count: tournament.length,
+    data: tournament
+  });
+});
