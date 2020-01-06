c03d922a7829a981c4b4387875e28b470c9f7733
Randy Burnet <rrw.burnet@gmail.com>
2020-01-06 01:21:38 +0100

    testing handlebars

--- config/handlebars.js
+++ config/handlebars.js
@@ -1,12 +1,11 @@
 const handlebars = require( 'express-handlebars');
- 
 const path = __dirname + '/../views/';
  
 const hb = handlebars({
             defaultLayout:"main",
             extname:"hbs",
             layoutsDir:path + "layouts",
-            partialsDir:path + "common"
+            partialsDir:path + "partials"
         });
  
 module.exports = hb;
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const Tournament = require('../models/Tournament');
+
 /* GET home page. */
 router.get('/', async (req, res, next) => {
   const tournament = await Tournament.find();
+  const sum = (a, b) => a * b;
 
-  res.render('layouts/main',{ title: 'Express', someVariable: 'example', count: tournament.length });
+  res.render('layouts/main', {
+    title: 'BMU TOOL',
+    someVariable: 'example',
+    count: tournament.length,
+    sum: sum(4, 5)
+  });
 });
 
 module.exports = router;
